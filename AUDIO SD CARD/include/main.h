f09835cb2ab78b431d782ce8251cdc5ad94d009e
BuiThien <bui.trong.thien.92@gmail.com>
2021-09-25 21:33:27 +0700

    Add Library SD card

---
+++ AUDIO SD CARD/include/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <Arduino.h>
+#include "SPI.h"
+#include "t3c_sd.h"
+
+#endif
--- AUDIO SD CARD/src/main.cpp
+++ AUDIO SD CARD/src/main.cpp
@@ -1,7 +1,26 @@
-#include <Arduino.h>
+#include "main.h"
 
-void setup() {
+mSD_Class SDcard;
+File fil;
+uint8_t buf[20];
+void setup() 
+{
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  if (!SDcard.mSD_begin(5)) 
+  {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  SDcard.mSD_open(&fil,"/chan ai.wav",FILE_READ);
+  SDcard.mSD_seek(&fil,10);
+  SDcard.mSD_read(&fil,buf,20);
+  SDcard.mSD_close(&fil);
+  for(int i=0;i<20;i++)
+  {
+    Serial.print(buf[i],HEX);
+    Serial.print(' ');
+  }
 }
 
 void loop() {
